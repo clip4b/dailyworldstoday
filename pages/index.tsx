b2680344873bad962b9f5585a074b93813d306a8
clip4b <131498097+clip4b@users.noreply.github.com>
2023-04-23 14:27:51 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,3 +3,13 @@ const Home: NextPage = () => {
  return ( <p>Hello</p> )
 }
 export default Home
+import { NextPage } from "next"
+import { useRouter } from "next/router"
+import { withLayout } from "../components/Layout"
+
+const Post: NextPage = () => {
+  const router = useRouter()
+
+  return <h1>Post: {router.query.title}</h1>
+}
+export default withLayout(Post)
