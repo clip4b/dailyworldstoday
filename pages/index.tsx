2558b56fcd33c65307d0f27be621bbdc14853941
clip4b <131498097+clip4b@users.noreply.github.com>
2023-04-23 14:33:18 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,45 @@
 import { NextPage } from "next"
-import { useRouter } from "next/router"
-import { withLayout } from "../../components/Layout"
+import Link from "next/link"
+import { withLayout } from "../components/Layout"
+import useSWR from "swr"
 
-const Post: NextPage = () => {
-  const router = useRouter()
-
-  return <h1>Post: {router.query.id}</h1>
+const fetcher = async (url: string) => {
+  const res = await fetch(url)
+  return (await res.json()) as { name: string }
 }
-export default withLayout(Post)
+
+const PostLink = ({ id }) => (
+  <li>
+    <Link href="/post/[id]" as={`/post/${id}`}>
+      <a>{id}</a>
+    </Link>
+  </li>
+)
+
+const Index: NextPage = () => {
+  const { data, error } = useSWR("/api/data", fetcher)
+
+  let name = data?.name
+
+  if (!data) name = "Loading..."
+  if (error) name = "Failed to fetch data."
+
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <h2>{name}</h2>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+      <ol>
+        <PostLink id="First-Post"></PostLink>
+        <PostLink id="Second-Post"></PostLink>
+        <PostLink id="Third-Post"></PostLink>
+      </ol>
+    </>
+  )
+}
+
+export default withLayout(Index)
