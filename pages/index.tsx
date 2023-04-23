f4fab982722110c0ff5a2b4e5530075fe80e3bf2
clip4b <131498097+clip4b@users.noreply.github.com>
2023-04-23 14:34:34 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,24 @@
-import { GetServerSidePropsResult } from "next";
+import { InferGetServerSidePropsType, NextPage } from "next";
+import { ReactChild } from "react";
 
-interface Props {
-  data: string;
-}
 
-const Test = ({ data }: Props) => {
-  return <div>{data}</div>;
+
+const Test: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    if (data) {
+        console.log(data);
+    }
+  return <div>ok welcome to /test route </div>;
 };
 
-export async function getServerSideProps(): Promise<GetServerSidePropsResult<Props>> {
-  const res = await fetch(`http://localhost:3000/api/hello`).then(
-    (res) => res.json()
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/api/hello`).then((res) =>
+    res.json()
   );
 
+  console.log(res);
   return {
-    props: { data: res },
+    props: { data: res }, // will be passed to the page component as props
   };
 }
+
+export default Test;
