662efc066367c2e60106bb95da58e89aa33870e3
clip4b <131498097+clip4b@users.noreply.github.com>
2023-04-23 14:34:06 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,19 @@
-import { NextPage } from "next"
-import Link from "next/link"
-import { withLayout } from "../components/Layout"
-import useSWR from "swr"
+import { GetServerSidePropsResult } from "next";
 
-const fetcher = async (url: string) => {
-  const res = await fetch(url)
-  return (await res.json()) as { name: string }
+interface Props {
+  data: string;
 }
 
-const PostLink = ({ id }) => (
-  <li>
-    <Link href="/post/[id]" as={`/post/${id}`}>
-      <a>{id}</a>
-    </Link>
-  </li>
-)
+const Test = ({ data }: Props) => {
+  return <div>{data}</div>;
+};
 
-const Index: NextPage = () => {
-  const { data, error } = useSWR("/api/data", fetcher)
+export async function getServerSideProps(): Promise<GetServerSidePropsResult<Props>> {
+  const res = await fetch(`http://localhost:3000/api/hello`).then(
+    (res) => res.json()
+  );
 
-  let name = data?.name
-
-  if (!data) name = "Loading..."
-  if (error) name = "Failed to fetch data."
-
-  return (
-    <>
-      <h1>Hello world!</h1>
-      <h2>{name}</h2>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-      <ol>
-        <PostLink id="First-Post"></PostLink>
-        <PostLink id="Second-Post"></PostLink>
-        <PostLink id="Third-Post"></PostLink>
-      </ol>
-    </>
-  )
+  return {
+    props: { data: res },
+  };
 }
-
-export default withLayout(Index)
